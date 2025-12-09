1b9deb4825896625516da2336f99af761c5cc71f
khonager <khonager@gmail.com>
2025-12-09 07:43:48 +0100

    first deploy

--- flake.nix
+++ flake.nix
@@ -1,76 +1,55 @@
 {
-  description = "NixOS Configuration for Framework 13 & PC Titan";
+  description = "Mondlicons - Smooth, vibrant curves by Khonager";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
-    stylix.url = "github:nix-community/stylix";
-    nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    # Custom inputs
-    nixos-conf-editor.url = "github:snowfallorg/nixos-conf-editor";
-    nix-software-center.url = "github:snowfallorg/nix-software-center";
-
-    zen-browser = {
-      url = "github:0xc000022070/zen-browser-flake";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        home-manager.follows = "home-manager";
-      };
-    };
-
-    # 👇 YOUR LOCAL ICON THEME
-    my-icons.url = "path:/home/jl/Documents/GitHub/my-icon-theme";
-  };
-
-  outputs =
-    { self
-    , nixpkgs
-    , nixos-hardware
-    , home-manager
-    , ...
-    }@inputs:
+  outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in
     {
-      formatter.${system} = pkgs.nixpkgs-fmt;
+      packages.${system}.default = pkgs.stdenv.mkDerivation {
+        name = "mondlicons";
+        src = ./.;
+        installPhase = ''
+                    # 1. Create the specific theme folder
+                    mkdir -p $out/share/icons/Mondlicons
+                    cp -r * $out/share/icons/Mondlicons/
+          
+                    # 2. Generate the metadata file
+                    cat > $out/share/icons/Mondlicons/index.theme <<CONFIG
+          [Icon Theme]
+          Name=Mondlicons
+          Comment=Smooth, vibrant curves.
+          Inherits=Papirus-Dark,Adwaita,hicolor
+          Directories=scalable/apps,48x48/apps,scalable/places,48x48/places
 
-      nixosConfigurations = {
-        framework = nixpkgs.lib.nixosSystem {
-          specialArgs = { inherit inputs self; };
-          modules = [
-            ./hosts/framework/configuration.nix
-            nixos-hardware.nixosModules.framework-amd-ai-300-series
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.extraSpecialArgs = { inherit inputs; };
-              home-manager.backupFileExtension = "backup";
-            }
-          ];
-        };
+          [scalable/apps]
+          Size=128
+          Type=Scalable
+          MinSize=16
+          MaxSize=512
+          Context=Applications
 
-        titan = nixpkgs.lib.nixosSystem {
-          specialArgs = { inherit inputs self; };
-          modules = [
-            ./hosts/titan/configuration.nix
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.extraSpecialArgs = { inherit inputs; };
-              home-manager.backupFileExtension = "backup";
-            }
-          ];
-        };
+          [48x48/apps]
+          Size=48
+          Type=Fixed
+          Context=Applications
+
+          [scalable/places]
+          Size=128
+          Type=Scalable
+          MinSize=16
+          MaxSize=512
+          Context=Places
+
+          [48x48/places]
+          Size=48
+          Type=Fixed
+          Context=Places
+          CONFIG
+        '';
       };
     };
 }
