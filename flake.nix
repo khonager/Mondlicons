ac7d9981f6e32c3499e6ae88b0393c44603cb964
khonager <khonager@gmail.com>
2025-12-09 07:14:44 +0100

    make icon theme part of system

--- flake.nix
+++ flake.nix
@@ -1,54 +1,76 @@
 {
-  description = "Khonager's Hand-Drawn Icon Theme";
+  description = "NixOS Configuration for Framework 13 & PC Titan";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    stylix.url = "github:nix-community/stylix";
+    nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
 
-  outputs = { self, nixpkgs }:
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # Custom inputs
+    nixos-conf-editor.url = "github:snowfallorg/nixos-conf-editor";
+    nix-software-center.url = "github:snowfallorg/nix-software-center";
+
+    zen-browser = {
+      url = "github:0xc000022070/zen-browser-flake";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        home-manager.follows = "home-manager";
+      };
+    };
+
+    # 👇 YOUR LOCAL ICON THEME
+    my-icons.url = "path:/home/jl/Documents/GitHub/my-icon-theme";
+  };
+
+  outputs =
+    { self
+    , nixpkgs
+    , nixos-hardware
+    , home-manager
+    , ...
+    }@inputs:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in
     {
-      packages.${system}.default = pkgs.stdenv.mkDerivation {
-        name = "my-icon-theme";
-        src = ./.;
-        installPhase = ''
-          mkdir -p $out/share/icons/MyDreamTheme
-          cp -r * $out/share/icons/MyDreamTheme/
-          
-          # Create the index.theme dynamically
-          cat > $out/share/icons/MyDreamTheme/index.theme <<CONFIG
-[Icon Theme]
-Name=MyDreamTheme
-Comment=Hand-drawn by Khonager
-Inherits=Papirus-Dark,Adwaita,hicolor
-Directories=scalable/apps,48x48/apps,scalable/places,48x48/places
+      formatter.${system} = pkgs.nixpkgs-fmt;
 
-[scalable/apps]
-Size=128
-Type=Scalable
-MinSize=16
-MaxSize=512
-Context=Applications
+      nixosConfigurations = {
+        framework = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs self; };
+          modules = [
+            ./hosts/framework/configuration.nix
+            nixos-hardware.nixosModules.framework-amd-ai-300-series
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.extraSpecialArgs = { inherit inputs; };
+              home-manager.backupFileExtension = "backup";
+            }
+          ];
+        };
 
-[48x48/apps]
-Size=48
-Type=Fixed
-Context=Applications
-
-[scalable/places]
-Size=128
-Type=Scalable
-MinSize=16
-MaxSize=512
-Context=Places
-
-[48x48/places]
-Size=48
-Type=Fixed
-Context=Places
-CONFIG
-        '';
+        titan = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs self; };
+          modules = [
+            ./hosts/titan/configuration.nix
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.extraSpecialArgs = { inherit inputs; };
+              home-manager.backupFileExtension = "backup";
+            }
+          ];
+        };
       };
     };
 }
